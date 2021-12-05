c8714c31367ffd8a311ab3d911e8a0859ff12f50
jiro <jiro853141@gmail.com>
2021-12-05 20:05:56 +0800

    feat: initial project

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  // 入口
+  entry: "./src/index.js",
+  // 模式 development
+  mode: "production",
+  // 出口
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.[hash].js",
+  },
+  // loader
+  module: {
+    rules: [
+      {
+        test: /\.css|\.scss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: "postcss-loader",
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(jpg|gif|png)/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  // 插件
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "index.[hash].css",
+    }),
+    new CleanWebpackPlugin(),
+    new CompressionPlugin(),
+  ],
+  devtool: "source-map",
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+};
